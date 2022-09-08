336a3abd64dd4b32e0844c6358fe526d2e25b968
Akhilesh Sharma <akhilesh.sharma168@gmail.com>
2022-09-08 11:03:23 +0530

    process.env. reverted

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const AxiosApi = axios.create({
   baseURL:
-    env.process.VERCEL_URL ||
-    env.process.REACT_APP_BASEURL ||
+    process.env.VERCEL_URL ||
+    process.env.REACT_APP_BASEURL ||
     'http://localhost:5000',
   headers: {
     'Content-Type': 'application/json',
